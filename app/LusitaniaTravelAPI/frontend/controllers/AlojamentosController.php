a739c570a99a4e59fd30f873bf088ba95e64dfaf
Maria Inês <92606230+mariainesgj@users.noreply.github.com>
2024-01-04 16:08:33 +0000

    Atualização 04/jan

--- app/LusitaniaTravelAPI/frontend/controllers/AlojamentosController.php
+++ app/LusitaniaTravelAPI/frontend/controllers/AlojamentosController.php
@@ -2,16 +2,55 @@
 
 namespace frontend\controllers;
 
+use common\models\Fornecedor;
+use common\models\Comentario;
+
 class AlojamentosController extends \yii\web\Controller
 {
-    public function actionIndex()
+
+    public function actionListarFornecedoresHoteis()
     {
-        return $this->render('index');
+        $query = Fornecedor::find()->where(['tipo' => 'Hotel']);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider->getModels();
     }
 
-    public function actionView()
+    public function actionListarAlojamentosLisboa()
     {
-        return $this->render('view');
+        $query = Fornecedor::find()->where(['localizacao' => 'Lisboa']);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider->getModels();
     }
+
+    public function actionComentariosFornecedorData($id = 1, $data = '2023-12-12')
+    {
+        $query = Comentario::find()->where(['fornecedor_id' => $id, 'data' => $data]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider->getModels();
+    }
+
+    public function actionListarAvaliacoesFornecedor($id = 1)
+    {
+        $query = Avaliacao::find()->where(['fornecedor_id' => $id]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider->getModels();
+    }
+    
 
 }
--- app/LusitaniaTravelAPI/frontend/controllers/ReservasController.php
+++ app/LusitaniaTravelAPI/frontend/controllers/ReservasController.php
@@ -2,21 +2,68 @@
 
 namespace frontend\controllers;
 
+use common\models\Reserva;
+
 class ReservasController extends \yii\web\Controller
 {
-    public function actionIndex()
+    public $user = null;
+    public $modelClass = 'common\models\Reserva';
+
+    public function actionReservasConfirmadas()
     {
-        return $this->render('index');
+        $query = Reserva::find()->where(['confirmada' => true]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider->getModels();
     }
 
-    public function actionView()
+    public function actionConfirmarReserva($reservaId)
     {
-        return $this->render('view');
+        $reserva = Reserva::findOne($reservaId);
+
+        if (!$reserva) {
+            throw new \yii\web\NotFoundHttpException('Reserva não encontrada.');
+        }
+
+        $reserva->confirmada = true; //Atualiza o estado da reserva
+
+        if ($reserva->save()) {
+            return ['success' => true, 'message' => 'Reserva confirmada com sucesso.'];
+        } else {
+            return ['success' => false, 'message' => 'Erro ao confirmar reserva.'];
+        }
     }
 
-    public function actionCreate()
+    public function actionCancelarReserva($reservaId)
     {
-        return $this->render('create');
+        $reserva = Reserva::findOne($reservaId);
+
+        if (!$reserva) {
+            throw new \yii\web\NotFoundHttpException('Reserva não encontrada.');
+        }
+
+        $reserva->status = 'Cancelada';
+
+        if ($reserva->save()) {
+            return ['success' => true, 'message' => 'Reserva cancelada com sucesso.'];
+        } else {
+            return ['success' => false, 'message' => 'Erro ao cancelar reserva.'];
+        }
     }
+
+    public function actionTaxaDeReservas()
+    {
+        $query = Reserva::find();
+
+        //Introduzir calculos da taxa
+
+        $taxaTotal = $query->sum('valor_total'); // Supondo que 'valor_total' seja o campo que representa o valor total da reserva
+
+        return ['taxa_total' => $taxaTotal];
+    }
+
 
 }
